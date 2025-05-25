6e16bd9e510543a151b000bf5d852dc66a15b9f9
Code_claude <codeclaude2@gmail.com>
2025-05-25 19:05:31 +0200

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -1,23 +1,13 @@
 /* #include <stdio.h> */
-
+// code is short now
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size = 0;
-	while ((s1[size] != '\0' || s2[size] != '\0') && size < n)
+	while (*s1 == *s2 && --n && *s1)
 	{
-		if (s1[size] > s2[size])
-		{
-			return (1);
-		}
-		else if (s1[size] < s2[size])
-		{
-			return (-1);
-		}
-		size++;
+		*s1++, *s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
-
 /* int main()
 {
 	char s1[] = "heLLO World!";
